8f80133b245abda84435f3b78e503dbc66d343e5
Gabe Sibley <gsibley@gwu.edu>
2012-03-25 19:02:15 +0000

    removed dead code from Demo.cpp
    git-svn-id: svn+ssh://rabbit.cs.gwu.edu/home/robotgames/repo/branch/SimpleGui@108 4610833d-7e64-4530-9b05-e6b80970a362

--- Examples/Demo.cpp
+++ Examples/Demo.cpp
@@ -28,34 +28,3 @@ int main( int argc, char** argv )
     return( pWin->Run() );
 }
 
-
-/*
-////////////////////////////////////////////////////////////////////////////
-
-int main( int argc, char** argv )
-{
-    GetPot cl(argc,argv);
-
-    std::string sMesh = cl.follow( "Ramp.STL", 1, "-mesh" );
-
-	// init window
-    GuiWindow* pWin = new GuiWindow( 0, 0, 1024, 768, "Simple Gui Demo" );
-
-    // load mesh
-    const struct aiScene* pScene;
-    struct aiLogStream stream = aiGetPredefinedLogStream( aiDefaultLogStream_STDOUT, NULL );
-    aiAttachLogStream( &stream );
-    pScene = aiImportFile( sMesh.c_str(), aiProcessPreset_TargetRealtime_MaxQuality );
-
-    GLMesh mesh;
-    GLGrid grid;
-
-    mesh.Init( pScene );
-
-	// register objects
-	pWin->AddChildToRoot( &grid );
-    pWin->AddChildToRoot( &mesh );
-
-    return( pWin->Run() );
-}
-*/
