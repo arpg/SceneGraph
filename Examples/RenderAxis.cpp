00e4a182779f74b3facaa5203b72256f5f8afb3a
Eli Mattson <emattson@gwmail.gwu.edu>
2012-04-04 17:34:48 +0000

    Fixed the coloring of the rotation rings, and fixed several small inconsistencies in the model
    git-svn-id: svn+ssh://rabbit.cs.gwu.edu/home/robotgames/repo/branch/SimpleGui@153 4610833d-7e64-4530-9b05-e6b80970a362

--- Examples/RenderAxis.cpp
+++ Examples/RenderAxis.cpp
@@ -12,20 +12,12 @@ int main (int argc, char** argv){
     //init window
     GLWindow* pWin = new GLWindow(0, 0, 1024, 768, "Render Axis Demo");
 
-    //load meshes
-    const struct aiScene* pScene;
-
     //generate grid
     GLGrid grid;
     grid.SetPerceptable( false );
-
     pWin->AddChildToRoot( &grid );
 
-
     GLAxis axis1;
-    axis1.SetPerceptable( false );
-
-
     pWin->AddChildToRoot( &axis1 );
 
     return (pWin->Run());
--- SimpleGui/GLAxis.h
+++ SimpleGui/GLAxis.h
@@ -2,6 +2,7 @@
 #define GLAXIS_H
 
 #include <SimpleGui/GLObject.h>
+#include <math.h>
 
 ///////////////////////////////////////////////////////////////////////////////
 class GLAxis : public GLObject
@@ -11,6 +12,45 @@ public:
     GLAxis()
     {
         m_sObjectName = "Axis";
+    }
+
+    void drawCircle(float r, char axis)
+    {
+        const float d2r = 3.14159/180; //must be in radians
+        glBegin(GL_LINE_LOOP);
+
+        if (axis == 'x')
+        {
+            glColor3f(0.0, 1.0, 0.0); //green
+            for(int i = 0; i < 360; i++)
+            {
+                float theta = i * d2r;
+                glVertex3f(0.0, cos(theta)*r, sin(theta)*r);
+            }
+        }
+        else if (axis == 'y')
+        {
+            glColor3f(1.0, 0.0, 0.0); //red
+            for(int i = 0; i < 360; i++)
+            {
+                float theta = i * d2r;
+                glVertex3f(sin(theta)*r, 0.0, cos(theta)*r);
+            }
+        }
+
+        else //axis = z
+        {
+
+            glColor3f(0.0, 0.0, 1.0); //blue
+            for(int i = 0; i < 360; i++)
+            {
+                float theta = i * d2r;
+                glVertex3f(cos(theta)*r, sin(theta)*r, 0.0);
+            }
+        }
+
+        glEnd();
+
     }
 
     //dispview
@@ -26,26 +66,34 @@ public:
         glDisable( GL_LIGHTING );
 
         //draw lines
+        glLineWidth(5.0); //for some reason this freaks things out
         glBegin(GL_LINES);
-        glColor3f(0.0, 0.0, 1.0);//set color to blue
- //       glLineWidth(2.0);
-        glVertex3f(0.0, 0.0, 0.0);//origin
-        glVertex3f(0.0, 0.0, -1.0);//z-axis
 
+    //x-axis
         glColor3f(0.0, 1.0, 0.0); //green
         glVertex3f(0.0, 0.0, 0.0);
-        glVertex3f(-1.0, 0.0, 0.0); //x-axis
+        glVertex3f(-1.2, 0.0, 0.0);
 
+    //y-axis
         glColor3f(1.0, 0.0, 0.0); //red
         glVertex3f(0.0, 0.0, 0.0);
-        glVertex3f(0.0, -1.0, 0.0); //y-axis
+        glVertex3f(0.0, -1.2, 0.0);
+
+    //z-axis
+        glColor3f(0.0, 0.0, 1.0);//blue
+        glVertex3f(0.0, 0.0, 0.0);//origin
+        glVertex3f(0.0, 0.0, -1.2);
 
         glEnd(); //end draw lines
 
-        glPopMatrix(); //pop back to origin
-
         //draw rings
-        //glPushMatrix();
+        drawCircle(1.0, 'x');
+        drawCircle(1.0, 'y');
+        drawCircle(1.0, 'z');
+
+        glLineWidth(1.0);
+
+        glPopMatrix(); //pop back to origin
 
         glDisable( GL_BLEND );
 
