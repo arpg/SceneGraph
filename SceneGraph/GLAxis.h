e29e8d3200d7051b369f5606c6cd78e81126c7c0
Steven Lovegrove <stevenlovegrove@gmail.com>
2012-10-15 13:55:56 -0400

    GLAxis axis size.
    Signed-off-by: Steven Lovegrove <stevenlovegrove@gmail.com>

--- SceneGraph/GLAxis.h
+++ SceneGraph/GLAxis.h
@@ -11,27 +11,27 @@ class GLAxis : public GLObject
 {
 public:
 
-    GLAxis()
-      : GLObject("Axis")
+    GLAxis(float axisSize = 1.0f)
+        : GLObject("Axis"), m_axisSize(axisSize)
     {
         m_bPerceptable = false;
     }
 
-    static void DrawUnitAxis()
+    static inline void DrawAxis(float size = 1.0f)
     {
         // draw axis
         glBegin(GL_LINES);
         glColor4f(1, 0, 0, 1);
         glVertex3d(0, 0, 0);
-        glVertex3d(1, 0, 0);
+        glVertex3d(size, 0, 0);
 
         glColor4f(0, 1, 0, 1);
         glVertex3d(0, 0, 0);
-        glVertex3d(0, 1, 0);
+        glVertex3d(0, size, 0);
 
         glColor4f(0, 0, 1, 1);
         glVertex3d(0, 0, 0);
-        glVertex3d(0, 0, 1);
+        glVertex3d(0, 0, size);
         glEnd();
     }
 
@@ -39,9 +39,12 @@ public:
     {
         glPushAttrib(GL_ENABLE_BIT | GL_LINE_BIT);
         glDisable( GL_LIGHTING );
-        DrawUnitAxis();
+        DrawAxis(m_axisSize);
         glPopAttrib();
     }
+
+protected:
+    float m_axisSize;
 };
 
 } // SceneGraph
