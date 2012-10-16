3551a93c80add68a6a9d798f905bbd9f80523a0c
Steven Lovegrove <stevenlovegrove@gmail.com>
2012-10-16 00:22:15 -0400

    Factored  GetPosNormal into pangolin.
    Signed-off-by: Steven Lovegrove <stevenlovegrove@gmail.com>

--- SceneGraph/PangolinSceneGraphHandler.h
+++ SceneGraph/PangolinSceneGraphHandler.h
@@ -8,7 +8,6 @@ namespace SceneGraph
 
 struct HandlerSceneGraph : pangolin::Handler3D
 {
-
     HandlerSceneGraph(SceneGraph::GLSceneGraph& graph, pangolin::OpenGlRenderState& cam_state, pangolin::AxisDirection enforce_up=pangolin::AxisNone, float trans_scale=0.01f)
         : pangolin::Handler3D(cam_state,enforce_up, trans_scale), m_scenegraph(graph), m_grab_width(15) {}
 
@@ -67,35 +66,9 @@ struct HandlerSceneGraph : pangolin::Handler3D
         }
     }
 
-    void GetPosNormal(pangolin::View& view, int x, int y, Eigen::Vector3d& p, Eigen::Vector3d& P, Eigen::Vector3d& n)
-    {
-        const GLint viewport[4] = {view.v.l,view.v.b,view.v.w,view.v.h};
-        const pangolin::OpenGlMatrix proj = cam_state->GetProjectionMatrix();
-        const pangolin::OpenGlMatrix mv = cam_state->GetModelViewMatrix();
-
-        glReadBuffer(GL_FRONT);
-        const int zl = (hwin*2+1);
-        const int zsize = zl*zl;
-        GLfloat zs[zsize];
-        glReadPixels(x-hwin,y-hwin,zl,zl,GL_DEPTH_COMPONENT,GL_FLOAT,zs);
-        const GLfloat mindepth = *(std::min_element(zs,zs+zsize));
-
-        p << x, y, mindepth;
-        gluUnProject(x, y, mindepth, mv.m, proj.m, viewport, &P(0), &P(1), &P(2));
-
-        Eigen::Vector3d Pl,Pr,Pb,Pt;
-        gluUnProject(x-hwin, y, zs[hwin*zl + 0],    mv.m, proj.m, viewport, &Pl(0), &Pl(1), &Pl(2));
-        gluUnProject(x+hwin, y, zs[hwin*zl + zl-1], mv.m, proj.m, viewport, &Pr(0), &Pr(1), &Pr(2));
-        gluUnProject(x, y-hwin, zs[hwin+1],         mv.m, proj.m, viewport, &Pb(0), &Pb(1), &Pb(2));
-        gluUnProject(x, y+hwin, zs[zsize-(hwin+1)], mv.m, proj.m, viewport, &Pt(0), &Pt(1), &Pt(2));
-
-        n = ((Pr-Pl).cross(Pt-Pb)).normalized();
-    }
-
     void Mouse(pangolin::View& view, pangolin::MouseButton button, int x, int y, bool pressed, int button_state)
     {
-        Eigen::Vector3d p, P, n;
-        GetPosNormal(view,x,y,p,P,n);
+        GetPosNormal(view,x,y,p,Pw,Pc,n);
 
         bool handled = false;
 
@@ -103,11 +76,16 @@ struct HandlerSceneGraph : pangolin::Handler3D
             m_selected_objects.clear();
             ComputeHits(view,*cam_state,x,y,m_grab_width,m_selected_objects);
             for(std::map<int,SceneGraph::GLObject*>::iterator i = m_selected_objects.begin(); i != m_selected_objects.end(); ++i ) {
-                handled |= i->second->Mouse(button, p, P, n, pressed, button_state, i->first);
+                handled |= i->second->Mouse( button,
+                    Eigen::Map<Eigen::Vector3d>(p), Eigen::Map<Eigen::Vector3d>(Pw), Eigen::Map<Eigen::Vector3d>(n),
+                    pressed, button_state, i->first
+                );
             }
         }else{
             for(std::map<int,SceneGraph::GLObject*>::iterator i = m_selected_objects.begin(); i != m_selected_objects.end(); ++i ) {
-                handled |= i->second->Mouse(button, p, P, n, pressed, button_state, i->first);
+                handled |= i->second->Mouse(button,
+                Eigen::Map<Eigen::Vector3d>(p), Eigen::Map<Eigen::Vector3d>(Pw), Eigen::Map<Eigen::Vector3d>(n),
+                pressed, button_state, i->first);
             }
         }
         if(!handled) {
@@ -117,13 +95,15 @@ struct HandlerSceneGraph : pangolin::Handler3D
 
     void MouseMotion(pangolin::View& view, int x, int y, int button_state)
     {
-        Eigen::Vector3d p, P, n;
-        GetPosNormal(view,x,y,p,P,n);
+        GetPosNormal(view,x,y,p,Pw,Pc,n);
 
         bool handled = false;
 
         for(std::map<int,SceneGraph::GLObject*>::iterator i = m_selected_objects.begin(); i != m_selected_objects.end(); ++i ) {
-            handled |= i->second->MouseMotion(p, P, n, button_state, i->first);
+            handled |= i->second->MouseMotion(
+                Eigen::Map<Eigen::Vector3d>(p), Eigen::Map<Eigen::Vector3d>(Pw), Eigen::Map<Eigen::Vector3d>(n),
+                button_state, i->first
+            );
         }
 
         if(!handled) {
@@ -135,8 +115,7 @@ struct HandlerSceneGraph : pangolin::Handler3D
     {
         // TODO: Implement Special handler for GLObjects too
 
-        Eigen::Vector3d p, P, n;
-        GetPosNormal(view,x,y,p,P,n);
+        GetPosNormal(view,x,y,p,Pw,Pc,n);
 
         bool handled = false;
 
@@ -146,7 +125,10 @@ struct HandlerSceneGraph : pangolin::Handler3D
 
             const pangolin::MouseButton button = p2 > 0 ? pangolin::MouseWheelUp : pangolin::MouseWheelDown;
             for(std::map<int,SceneGraph::GLObject*>::iterator i = m_selected_objects.begin(); i != m_selected_objects.end(); ++i ) {
-                handled |= i->second->Mouse(button, p, P, n, true, button_state, i->first);
+                handled |= i->second->Mouse(button,
+                    Eigen::Map<Eigen::Vector3d>(p), Eigen::Map<Eigen::Vector3d>(Pw), Eigen::Map<Eigen::Vector3d>(n),
+                    true, button_state, i->first
+                );
             }
         }
 
