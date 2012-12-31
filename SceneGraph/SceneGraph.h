7b79d39cb9d3e509efef578ffb3c4481f82b93ee
Gabe Sibley <gsibley@gwu.edu>
2012-12-30 23:58:19 -0500

    Added GLBox object

--- SceneGraph/SceneGraph.h
+++ SceneGraph/SceneGraph.h
@@ -8,6 +8,7 @@
 #include <SceneGraph/GLGrid.h>
 #include <SceneGraph/GLAxis.h>
 #include <SceneGraph/GLCube.h>
+#include <SceneGraph/GLBox.h>
 #include <SceneGraph/GLAxisAlignedBox.h>
 #include <SceneGraph/GLMovableAxis.h>
 #include <SceneGraph/GLWaypoint.h>
