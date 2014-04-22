89adc438ca78acfd5bb3bbd440681533a25515f1
Jack Morrison <jackmorrison1@gmail.com>
2014-04-22 10:14:45 -0400

    Cleans up GLColor.h

--- SceneGraph/GLColor.h
+++ SceneGraph/GLColor.h
@@ -1,118 +1,90 @@
-/*
-
-
- */
-
-#ifndef GLCOLOR_H__
-#define GLCOLOR_H__
+#ifndef _SCENEGRAPH_GLCOLOR_H__
+#define _SCENEGRAPH_GLCOLOR_H__
 
 #include <sstream>
 #include <cstdio>
+#include <cmath>
+#include <SceneGraph/GLinclude.h>
 
-namespace SceneGraph
-{
+namespace SceneGraph {
 
-////////////////////////////////////////////////////////////////////////////////
-struct GLColor
-{
-    static GLColor HsvColor( double hue, double s = 1.0, double v = 1.0 )
-    {
-      hue *= 360.0;
-      const double h = hue / 60.0;
-      const int i = floor(h);
-      const double f = (i%2 == 0) ? 1-(h-i) : h-i;
-      const double m = v * (1-s);
-      const double n = v * (1-s*f);
-      GLColor color;
-      switch(i)
-      {
-      case 0: color = GLColor(v,n,m,1.0f); break;
-      case 1: color = GLColor(n,v,m,1.0f); break;
-      case 2: color = GLColor(m,v,n,1.0f); break;
-      case 3: color = GLColor(m,n,v,1.0f); break;
-      case 4: color = GLColor(n,m,v,1.0f); break;
-      case 5: color = GLColor(v,m,n,1.0f); break;
+struct GLColor {
+  static GLColor HsvColor(double hue, double s = 1.0, double v = 1.0) {
+    hue *= 360.0;
+    const double h = hue / 60.0;
+    const int i = floor(h);
+    const double f = (i % 2 == 0) ? 1 - (h - i) : h - i;
+    const double m = v * (1-s);
+    const double n = v * (1-s*f);
+    GLColor color;
+    switch(i) {
+      case 0: color = GLColor(v, n, m, 1.0f); break;
+      case 1: color = GLColor(n, v, m, 1.0f); break;
+      case 2: color = GLColor(m, v, n, 1.0f); break;
+      case 3: color = GLColor(m, n, v, 1.0f); break;
+      case 4: color = GLColor(n, m, v, 1.0f); break;
+      case 5: color = GLColor(v, m, n, 1.0f); break;
       default:
         break;
-      }
-      return color;
     }
+    return color;
+  }
 
+  GLColor(float tr = 1.0f, float tg = 1.0f, float tb = 1.0f, float ta = 1.0f) {
+    r = tr;
+    g = tg;
+    b = tb;
+    a = ta;
+  }
 
-    GLColor(
-            float tr = 1.0f,
-            float tg = 1.0f, 
-            float tb = 1.0f, 
-            float ta = 1.0f 
-           )
-    {
-        r = tr;
-        g = tg;
-        b = tb;
-        a = ta;
-    }
+  GLColor(int tr, int tg, int tb, int ta = 255) {
+    r = tr/255.0f;
+    g = tg/255.0f;
+    b = tb/255.0f;
+    a = ta/255.0f;
+  }
 
-    GLColor( 
-            int tr,
-            int tg,
-            int tb,
-            int ta = 255 
-           ) 
-    {
-        r = tr/255.0f; 
-        g = tg/255.0f;
-        b = tb/255.0f;
-        a = ta/255.0f;
-    }
+  void Apply() const {
+    glColor4f(r,g,b,a);
+  }
 
-    void Apply() {
-        glColor4f(r,g,b,a);
-    }
-
-    struct {
-        float r;
-        float g;
-        float b;
-        float a;
-    };
+  struct {
+    float r;
+    float g;
+    float b;
+    float a;
+  };
 };
-
 
 #ifndef NAME_MAX
 #   define NAME_MAX 1024
 #endif
 
-////////////////////////////////////////////////////////////////////////////////
 /// All types you wish to use with CVars must overload << and >>.
-inline std::ostream &operator<<(std::ostream &stream, GLColor &color)
-{
-    int r = int( 255*color.r );
-    int g = int( 255*color.g );
-    int b = int( 255*color.b );
-    int a = int( 255*color.a );
+inline std::ostream &operator<<(std::ostream &stream, GLColor &color) {
+  int r = int(255*color.r);
+  int g = int(255*color.g);
+  int b = int(255*color.b);
+  int a = int(255*color.a);
 
-    stream << "[ " << r << ",  " << g << ",  " << b << ",  " << a << " ]";
-    return stream;
+  stream << "[ " << r << ",  " << g << ",  " << b << ",  " << a << " ]";
+  return stream;
 }
 
-////////////////////////////////////////////////////////////////////////////////
 /// All types you wish to use with CVars must overload << and >>.
-inline std::istream &operator>>(std::istream &stream, GLColor &color)
-{
-    int r=0,g=0,b=0,a=0;
+inline std::istream &operator>>(std::istream &stream, GLColor &color) {
+  int r = 0, g = 0, b = 0, a = 0;
 
-    char str[NAME_MAX] = {0};
-    stream.readsome( str, NAME_MAX );
-    sscanf( str, "[ %d, %d, %d, %d ]", &r, &g, &b, &a );
+  char str[NAME_MAX] = {0};
+  stream.readsome(str, NAME_MAX);
+  sscanf(str, "[ %d, %d, %d, %d ]", &r, &g, &b, &a);
 
-    color.r = (float)r/255.0f;
-    color.g = (float)g/255.0f;
-    color.b = (float)b/255.0f;
-    color.a = (float)a/255.0f;
- 
-    return stream;
+  color.r = (float)r/255.0f;
+  color.g = (float)g/255.0f;
+  color.b = (float)b/255.0f;
+  color.a = (float)a/255.0f;
+
+  return stream;
 }
-
-} // SceneGraph
-
+} // namespace SceneGraph
 #endif
