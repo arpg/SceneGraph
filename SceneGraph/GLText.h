6df776cc97aad66412ee81ebdea52b2b8a090970
Jack Morrison <jackmorrison1@gmail.com>
2014-06-11 10:02:45 -0400

    Cleans up GLText, adds set_color

--- SceneGraph/GLText.h
+++ SceneGraph/GLText.h
@@ -9,97 +9,81 @@
 
 #include <string>
 
-namespace SceneGraph
-{
+namespace SceneGraph {
 
-class GLText : public GLObject
-{
-public:
+class GLText : public GLObject {
+ public:
+  static pangolin::GlFont& GetFont() {
+    static pangolin::GlFont s_font;
+    return s_font;
+  }
 
-    static pangolin::GlFont& GetFont()
-    {
-        static pangolin::GlFont s_font;
-        return s_font;
+  /////////////////////////////////
+  // Static Utilities for drawing text
+  /////////////////////////////////
+
+  /// Render text at current glRasterPos
+  static void Draw(const std::string& text) {
+    pangolin::GlText txt = GetFont().Text(text.c_str());
+    txt.Draw();
+  }
+
+  /// Render text at (x,y,z)
+  static void Draw(const std::string& text, float x, float y, float z = 0) {
+    pangolin::GlText txt = GetFont().Text(text.c_str());
+    txt.Draw(x,y,z);
+  }
+
+  /// Return width (in pixels) of text
+  static int Width(const std::string& text) {
+    pangolin::GlText txt = GetFont().Text(text.c_str());
+    return txt.Width();
+  }
+
+  /////////////////////////////////
+  // GLText constructors
+  /////////////////////////////////
+
+  GLText(std::string text = "", double x = 0, double y = 0, double z = 0)
+      : GLObject("Text: " + text), m_sText(text) {
+    m_bPerceptable = false;
+    SetPosition(x,y,z);
+  }
+
+  /////////////////////////////////
+  // GLText member functions
+  /////////////////////////////////
+
+  void DrawCanonicalObject() {
+    if(m_gltext.Text() != m_sText) {
+      // Reinitialise text
+      m_gltext = GetFont().Text(m_sText.c_str());
     }
 
-    /////////////////////////////////
-    // Static Utilities for drawing text
-    /////////////////////////////////
+    m_Color.Apply();
+    pangolin::GlState gl;
+    gl.glDisable(GL_DEPTH_TEST);
+    gl.glDisable(GL_LIGHTING);
+    Draw(m_sText, 0, 0, 0);
+  }
 
-    /// Render text at current glRasterPos
-    static void Draw(const std::string& text )
-    {
-        pangolin::GlText txt = GetFont().Text(text.c_str());
-        txt.Draw();
-    }
+  void SetText(const std::string& sText) {
+    m_sText = sText;
+  }
 
-    /// Render text at (x,y,z)
-    static void Draw(const std::string& text, float x, float y, float z = 0 )
-    {
-        pangolin::GlText txt = GetFont().Text(text.c_str());
-        txt.Draw(x,y,z);
-    }
+  void ClearText() {
+    m_sText.clear();
+  }
 
-    /// Return width (in pixels) of text
-    static int Width(const std::string& text )
-    {
-        pangolin::GlText txt = GetFont().Text(text.c_str());
-        return txt.Width();
-    }
+  void set_color(const GLColor& color) {
+    m_Color = color;
+  }
 
-    /////////////////////////////////
-    // GLText constructors
-    /////////////////////////////////
-
-    GLText(std::string text="", double x = 0, double y = 0, double z = 0)
-        : GLObject("Text: " + text), m_sText(text)
-    {
-        m_bPerceptable = false;
-        SetPosition(x,y,z);
-    }
-
-    /////////////////////////////////
-    // GLText member functions
-    /////////////////////////////////
-
-    void DrawCanonicalObject()
-    {
-        if(m_gltext.Text() != m_sText ) {
-            // Reinitialise text
-            m_gltext = GetFont().Text(m_sText.c_str());
-        }
-
-        m_Color.Apply();
-        pangolin::GlState gl;
-        gl.glDisable(GL_DEPTH_TEST);
-        gl.glDisable(GL_LIGHTING);
-        Draw(m_sText, 0,0,0);
-    }
-
-    void SetText( const std::string& sText )
-    {
-        m_sText = sText;
-    }
-
-    void ClearText()
-    {
-        m_sText.clear();
-    }
-
-    void SetSize( int /*nFontSize*/ )
-    {
-        // uncomment this assertion again when variable is used
-        // assert(nFontSize > 0);
-        // TODO: support multiple sized.
-    }
-
-private:
-    std::string 	 m_sText;
-    pangolin::GlText m_gltext;
-    GLColor          m_Color;
+ private:
+  std::string m_sText;
+  pangolin::GlText m_gltext;
+  GLColor m_Color;
 };
-
 } // SceneGraph
-
 
 #endif
