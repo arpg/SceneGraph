7ff8101de09775181711320dd0cffc90df96aa3b
crheckman <crheckman@gmail.com>
2015-10-27 16:55:28 -0600

    Support arbitrary-size Eigen objects.

--- Examples/ModelPhysics/VarHelpers.h
+++ Examples/ModelPhysics/VarHelpers.h
@@ -4,17 +4,12 @@
 #include <vector>
 #include <Eigen/Core>
 
-namespace Eigen
-{
-typedef Matrix<double,5,1> Vector5d;
-typedef Matrix<double,6,1> Vector6d;
-}
-
 ////////////////////////////////////////////////////////////////////////////
 // Overloading Eigen for Pangolin Vars
 ////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
-inline std::ostream& operator<<( std::ostream& Stream, Eigen::Vector3d& Mat )
+template<typename T, int R, int C>
+inline std::ostream& operator<<( std::ostream& Stream, const Eigen::Matrix<T,R,C>& Mat )
 {
   unsigned int nRows = Mat.rows();
   unsigned int nCols = Mat.cols();
@@ -40,7 +35,8 @@ inline std::ostream& operator<<( std::ostream& Stream, Eigen::Vector3d& Mat )
 }
 
 ////////////////////////////////////////////////////////////////////////////
-inline std::istream& operator>>( std::istream& Stream, Eigen::Vector3d& Mat )
+template<typename T, int R, int C>
+inline std::istream& operator>>( std::istream& Stream, Eigen::Matrix<T,R,C>& Mat )
 {
 
   unsigned int nRows = Mat.rows();
@@ -54,284 +50,17 @@ inline std::istream& operator>>( std::istream& Stream, Eigen::Vector3d& Mat )
   for( unsigned int ii = 0; ii < nRows-1; ii++ ) {
     for( unsigned int jj = 0; jj < nCols-1; jj++ ) {
       Stream.getline(str, 255, ',');
-      Mat(ii, jj) = std::strtod(str, NULL);
+      Mat(ii, jj) = (T)std::strtod(str, NULL);
     }
     Stream.getline(str, 255, ';');
-    Mat(ii, nCols-1) = std::strtod(str, NULL);
+    Mat(ii, nCols-1) = (T)std::strtod(str, NULL);
   }
   for( unsigned int jj = 0; jj < nCols-1; jj++ ) {
     Stream.getline(str, 255, ',');
-    Mat(nRows-1, jj) = std::strtod(str, NULL);
+    Mat(nRows-1, jj) = (T)std::strtod(str, NULL);
   }
   Stream.getline(str, 255, ']');
-  Mat(nRows-1, nCols-1) = std::strtod(str, NULL);
-  return Stream;
-}
-
-////////////////////////////////////////////////////////////////////////////
-inline std::ostream& operator<<(std::ostream& Stream, std::vector<Eigen::Vector3d>& vEigen ) {
-  if( vEigen.size() == 0 ) {
-    Stream << "[ ]";
-    return Stream;
-  }
-
-  Stream << "[ " << vEigen[0];
-  for( size_t i=1; i<vEigen.size(); i++ ) {
-    Stream << " " << vEigen[i];
-  }
-  Stream << " ]";
-
-  return Stream;
-}
-
-////////////////////////////////////////////////////////////////////////////
-inline std::istream& operator>>(std::istream& Stream, std::vector<Eigen::Vector3d>& vEigen ) {
-
-  std::stringstream sBuf;
-  Eigen::Vector3d Mat;
-
-  vEigen.clear();
-
-  char str[256];
-
-  // strip first bracket
-  Stream.getline(str, 255, '[');
-  if( Stream.gcount() > 1 ) {
-    return Stream;
-  }
-
-  // get first element
-  Stream.getline(str, 255, '[');
-  Stream.getline(str, 255, ']');
-
-  while( Stream.gcount() > 1 ) {
-    sBuf << "[";
-    sBuf << str;
-    sBuf << "]";
-    sBuf >> Mat;
-    vEigen.push_back(Mat);
-
-    // get next element
-    Stream.getline(str, 255, '[');
-    Stream.getline(str, 255, ']');
-  }
-  return Stream;
-}
-
-////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-inline std::ostream& operator<<( std::ostream& Stream, Eigen::Vector5d& Mat )
-{
-  unsigned int nRows = Mat.rows();
-  unsigned int nCols = Mat.cols();
-
-  Stream << "[ ";
-
-  for( unsigned int ii = 0; ii < nRows-1; ii++ ) {
-    for( unsigned int jj = 0; jj < nCols-1; jj++ ) {
-      Stream << Mat(ii, jj);
-      Stream << ", ";
-    }
-    Stream << Mat(ii, nCols-1);
-    Stream << "; ";
-  }
-  for( unsigned int jj = 0; jj < nCols-1; jj++ ) {
-    Stream << Mat(nRows-1, jj);
-    Stream << ", ";
-  }
-  Stream << Mat(nRows-1, nCols-1);
-  Stream << " ]";
-
-  return Stream;
-}
-
-////////////////////////////////////////////////////////////////////////////
-inline std::istream& operator>>( std::istream& Stream, Eigen::Vector5d& Mat )
-{
-
-  unsigned int nRows = Mat.rows();
-  unsigned int nCols = Mat.cols();
-  char str[256];
-
-  Stream.getline(str, 255, '[');
-  if( Stream.gcount() > 1 ) {
-    return Stream;
-  }
-  for( unsigned int ii = 0; ii < nRows-1; ii++ ) {
-    for( unsigned int jj = 0; jj < nCols-1; jj++ ) {
-      Stream.getline(str, 255, ',');
-      Mat(ii, jj) = std::strtod(str, NULL);
-    }
-    Stream.getline(str, 255, ';');
-    Mat(ii, nCols-1) = std::strtod(str, NULL);
-  }
-  for( unsigned int jj = 0; jj < nCols-1; jj++ ) {
-    Stream.getline(str, 255, ',');
-    Mat(nRows-1, jj) = std::strtod(str, NULL);
-  }
-  Stream.getline(str, 255, ']');
-  Mat(nRows-1, nCols-1) = std::strtod(str, NULL);
-  return Stream;
-}
-
-////////////////////////////////////////////////////////////////////////////
-inline std::ostream& operator<<(std::ostream& Stream, std::vector<Eigen::Vector5d>& vEigen ) {
-  if( vEigen.size() == 0 ) {
-    Stream << "[ ]";
-    return Stream;
-  }
-
-  Stream << "[ " << vEigen[0];
-  for( size_t i=1; i<vEigen.size(); i++ ) {
-    Stream << " " << vEigen[i];
-  }
-  Stream << " ]";
-
-  return Stream;
-}
-
-////////////////////////////////////////////////////////////////////////////
-inline std::istream& operator>>(std::istream& Stream, std::vector<Eigen::Vector5d>& vEigen ) {
-
-  std::stringstream sBuf;
-  Eigen::Vector5d Mat;
-
-  vEigen.clear();
-
-  char str[256];
-
-  // strip first bracket
-  Stream.getline(str, 255, '[');
-  if( Stream.gcount() > 1 ) {
-    return Stream;
-  }
-
-  // get first element
-  Stream.getline(str, 255, '[');
-  Stream.getline(str, 255, ']');
-
-  while( Stream.gcount() > 1 ) {
-    sBuf << "[";
-    sBuf << str;
-    sBuf << "]";
-    sBuf >> Mat;
-    vEigen.push_back(Mat);
-
-    // get next element
-    Stream.getline(str, 255, '[');
-    Stream.getline(str, 255, ']');
-  }
-  return Stream;
-}
-
-
-////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-inline std::ostream& operator<<( std::ostream& Stream, Eigen::Vector6d& Mat )
-{
-  unsigned int nRows = Mat.rows();
-  unsigned int nCols = Mat.cols();
-
-  Stream << "[ ";
-
-  for( unsigned int ii = 0; ii < nRows-1; ii++ ) {
-    for( unsigned int jj = 0; jj < nCols-1; jj++ ) {
-      Stream << Mat(ii, jj);
-      Stream << ", ";
-    }
-    Stream << Mat(ii, nCols-1);
-    Stream << "; ";
-  }
-  for( unsigned int jj = 0; jj < nCols-1; jj++ ) {
-    Stream << Mat(nRows-1, jj);
-    Stream << ", ";
-  }
-  Stream << Mat(nRows-1, nCols-1);
-  Stream << " ]";
-
-  return Stream;
-}
-
-////////////////////////////////////////////////////////////////////////////
-inline std::istream& operator>>( std::istream& Stream, Eigen::Vector6d& Mat )
-{
-
-  unsigned int nRows = Mat.rows();
-  unsigned int nCols = Mat.cols();
-  char str[256];
-
-  Stream.getline(str, 255, '[');
-  if( Stream.gcount() > 1 ) {
-    return Stream;
-  }
-  for( unsigned int ii = 0; ii < nRows-1; ii++ ) {
-    for( unsigned int jj = 0; jj < nCols-1; jj++ ) {
-      Stream.getline(str, 255, ',');
-      Mat(ii, jj) = std::strtod(str, NULL);
-    }
-    Stream.getline(str, 255, ';');
-    Mat(ii, nCols-1) = std::strtod(str, NULL);
-  }
-  for( unsigned int jj = 0; jj < nCols-1; jj++ ) {
-    Stream.getline(str, 255, ',');
-    Mat(nRows-1, jj) = std::strtod(str, NULL);
-  }
-  Stream.getline(str, 255, ']');
-  Mat(nRows-1, nCols-1) = std::strtod(str, NULL);
-  return Stream;
-}
-
-////////////////////////////////////////////////////////////////////////////
-inline std::ostream& operator<<(std::ostream& Stream, std::vector<Eigen::Vector6d>& vEigen ) {
-  if( vEigen.size() == 0 ) {
-    Stream << "[ ]";
-    return Stream;
-  }
-
-  Stream << "[ " << vEigen[0];
-  for( size_t i=1; i<vEigen.size(); i++ ) {
-    Stream << " " << vEigen[i];
-  }
-  Stream << " ]";
-
-  return Stream;
-}
-
-////////////////////////////////////////////////////////////////////////////
-inline std::istream& operator>>(std::istream& Stream, std::vector<Eigen::Vector6d>& vEigen ) {
-
-  std::stringstream sBuf;
-  Eigen::Vector6d Mat;
-
-  vEigen.clear();
-
-  char str[256];
-
-  // strip first bracket
-  Stream.getline(str, 255, '[');
-  if( Stream.gcount() > 1 ) {
-    return Stream;
-  }
-
-  // get first element
-  Stream.getline(str, 255, '[');
-  Stream.getline(str, 255, ']');
-
-  while( Stream.gcount() > 1 ) {
-    sBuf << "[";
-    sBuf << str;
-    sBuf << "]";
-    sBuf >> Mat;
-    vEigen.push_back(Mat);
-
-    // get next element
-    Stream.getline(str, 255, '[');
-    Stream.getline(str, 255, ']');
-  }
+  Mat(nRows-1, nCols-1) = (T)std::strtod(str, NULL);
   return Stream;
 }
 
